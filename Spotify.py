6576ecf94cc705d37caeafbf51311200898136b4
Kwame Doonquah <doonkwa@gmail.com>
2025-02-27 22:41:36 -0500

    Initial commit. Spotify to YouTube integration

---
+++ Spotify.py
@@ -0,0 +1,147 @@
+import requests, json, pandas as pd, os
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+#Create definitions for the OAuth 2.0 configuration for both Spotify and YouTube 
+#Spotify OAuth 2.0 configuration
+
+
+class SpotifyAPI: 
+    def __init__(self): 
+        with open(os.getcwd()+r"\\API keys + refresh tokens\\" + "spotify_client_secret.json") as f:
+            Spotify_Creds = json.load(f)
+        self.redirect_uri = "https://localhost:8080"
+        self.auth_url = "https://accounts.spotify.com/api/token"
+        self.client_id = Spotify_Creds["web"]["client_id"]
+        self.client_secret = Spotify_Creds["web"]["client_secret"]
+        
+    #kwargs was used just in case another client_id, client_secret, etc... was desired to be used
+    #otherwise our default client_id, client_secret, etc... would be used 
+    def refresh_token(self, **kwargs) -> str:
+        client_id = kwargs.get('client_id',self.client_id)
+        client_secret = kwargs.get('client_secret',self.client_secret)
+        auth_url = kwargs.get('auth_url', self.auth_url)
+        client = BackendApplicationClient(client_id=client_id)
+        oauth = OAuth2Session(client=client)
+        token = oauth.fetch_token(token_url=auth_url, client_id=client_id,
+                                client_secret=client_secret)
+        return token["access_token"]
+
+    def get_playlist_id(self, url: str) -> str:
+        playlist_id = url.split('playlist/')[1]
+        return playlist_id
+    
+
+    def get_playlist_info(self, playlist_id: str) -> json:
+        import requests
+        headers = {"Authorization": f"Bearer {SpotifyAPI().refresh_token()}"}
+        response = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}?market=US", headers=headers).json()
+        # if response.code = 200 then:
+        # self.data['Name'] = response.json() 
+        return response
+
+    def get_playlist_df(self, playlist_id: str): 
+        import pandas as pd 
+        response = SpotifyAPI().get_playlist_info(playlist_id)
+        data = response['tracks']['items']
+        Spotify_dict = {} 
+        Spotify_dict["artist"] = []
+        Spotify_dict["artist_id"] = []
+        Spotify_dict["track"] = []
+        Spotify_dict["track_id"] = []
+        Spotify_dict["track URL"] = []
+        Spotify_dict["duration"] = []
+        Spotify_dict["date_added"] = []
+
+        for i in range(len(data)):
+            artist_id = data[i]["track"]["artists"][0]["id"]
+            artist_path = data[i]["track"]["artists"][0]["name"]
+            song_path = data[i]["track"]["name"]
+            song_id = data[i]["track"]["id"]
+            song_url = data[i]["track"]["external_urls"]["spotify"]
+            song_duration = data[i]["track"]["duration_ms"]
+            date_added = data[i]["added_at"][:10]
+            Spotify_dict["artist"].append(artist_path)
+            Spotify_dict["artist_id"].append(artist_id)
+            Spotify_dict["track"].append(song_path)
+            Spotify_dict["track_id"].append(song_id)
+            Spotify_dict["duration"].append(song_duration)
+            Spotify_dict["track URL"].append(song_url)
+            Spotify_dict["date_added"].append(date_added)
+            
+        df = pd.DataFrame(Spotify_dict, columns=Spotify_dict.keys())
+        return df
+
+import keyring, psycopg
+class DB: 
+#Create definitions for DB connections and actions 
+#connect to the database
+    def __init__(self):
+        try:
+            self.conn = psycopg.connect(dbname="postgres", user="postgres", password=keyring.get_password("postgres", "postgres"), host=keyring.get_password("postgres", "host"), port="5432", autocommit=True)
+            #self.conn.autocommit = True
+        except Exception as e:
+            print(e)
+        self.DB_cols = ['playlist_id', 'artist', 'artist_id', 'track', 'track_id','duration','date_added', 'video_id','playlist_list_id']
+
+#connect_to_db().cursor().execute("SELECT * from spotify.playlist_ref").fetchall()
+
+#if playlist reference table does not exist, create it. Making this a private method only because this should already be established
+    def create_ref_playlist_table(self):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("CREATE TABLE IF NOT EXISTS Spotify.playlist_ref (playlist_id VARCHAR(200) primary key, playlist_name VARCHAR(200))")
+        except Exception as e:
+            print(e)
+        cur.close()
+
+
+#insert new playlist into the playlist reference table
+    def insert_playlist_ref(self, playlist_id: str, playlist_name: str):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("INSERT INTO Spotify.playlist_ref (playlist_id, playlist_name) VALUES (%s, %s)", (playlist_id, playlist_name))
+        except Exception as e:
+            print(e)
+            pass    
+        cur.close()
+
+
+#Create a table for the playlist details
+    def create_playlist_table(self,playlist_id: str):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(f"CREATE TABLE IF NOT EXISTS Spotify.playlist_{playlist_id} (playlist_id varchar(200), artist varchar(200), artist_id varchar(200), track varchar(200), track_id varchar(200) primary key, duration INT, date_added DATE, video_id varchar(200), playlist_list_id VARCHAR(75))")
+        except Exception as e:
+            print(e)        
+        cur.close()
+
+#insert new songs into the playlist table
+    def insert_new_songs(self, playlist_id: str, new_songs: str):
+        new_songs['track'] = new_songs['track'].str.replace("'","''")
+        new_songs['artist'] = new_songs['artist'].str.replace("'","''")
+        with self.conn.cursor() as cur:
+            playlist_insert = f"INSERT INTO spotify.playlist_{playlist_id} "
+            for i in range(len(new_songs)):
+                try: 
+                    cur.execute(playlist_insert + f"""(playlist_id, artist, artist_id, track, track_id, duration, date_added) VALUES 
+                                ('{playlist_id}', '{new_songs["artist"][i]}', '{new_songs["artist_id"][i]}', '{new_songs["track"][i]}', '{new_songs["track_id"][i]}', {new_songs["duration"][i]}, '{new_songs["date_added"][i]}')""")
+                except Exception as e:
+                    print(e)
+                    pass
+        cur.close()
+
+#remove songs from playlist table 
+    def del_old_songs(self, playlist_id: str, track_id: str):
+        with self.conn.cursor() as cur: 
+            track_id_del = f"DELETE FROM spotify.playlist_{playlist_id} where track_id = '{track_id}'"
+            try: 
+                cur.execute(track_id_del)
+            except Exception as e: 
+                print(e)
+                pass
+        cur.close()
+
+#gather playlist_DB 
+    def get_playlist_df(self, playlist_id: str):
+        snapshot = pd.DataFrame(self.conn.cursor().execute(f"SELECT * from spotify.playlist_{playlist_id} ORDER BY date_added ASC").fetchall(), columns=self.DB_cols)
+        return snapshot
---
+++ YouTube.py
@@ -0,0 +1,153 @@
+import pandas as pd, requests, regex, json, requests, os
+#YouTube API configuration 
+
+# ---> YouTube OAuth 2.0 configuration 
+#Once we have the unique video id we will add the video to a private playlist 
+#This action will require OAuth 2.0 token because we are accessing/modifying private user data
+import googleapiclient.discovery,  google_auth_oauthlib.flow, googleapiclient.errors 
+from google.oauth2 import credentials
+
+
+
+class YouTubeAPI:
+    def __init__(self):
+        with open(os.getcwd()+r"\\API keys + refresh tokens\\" + "YouTubeAPI.json") as f: 
+            YouTube_API_json = json.load(f)
+        self.YouTube_API_key = YouTube_API_json['API']
+        self.KD_channel = 'UCvhiKXH8XeOo3cQePeFwjpg'
+        self.YT_playlist ='PL3qmZgAqUhzaEzdV_Cl9dhPn9K9B2CGTS'
+
+
+
+    def search(self, track, artist, duration):
+        if type(track) != str:
+            track = track.str.replace("'","''")
+        else:
+            track.replace("'","''")
+        if type(artist) != str:
+            artist = artist.str.replace("'","''")
+        else: 
+            artist = artist.replace("'","''")
+        #add search query parameters including the track name and artist and duration 
+        keyword = f"{track} {artist}".replace(" ", "+")
+        if duration/(1000*60) < 4:
+            run_time = "short"
+        else:
+            run_time = "medium"
+        YT_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q={keyword}&type=video&videoDuration={run_time}&key={self.YouTube_API_key}"
+        YT_response = requests.get(YT_url)
+        print(YT_response)
+        YT_dump = YT_response.json()
+            #Match the artist to the channel title if the channel title has the artist name in it
+        k = 0   
+        video_id = ''
+        while k < len(YT_dump["items"]):
+            channel_titles = YT_dump["items"][k]["snippet"]["channelTitle"]
+            track_names = YT_dump["items"][k]["snippet"]["title"]
+            artist_regex_pattern = f"({artist.lower()}){{e<=2}}"
+            artists_regex_match = regex.findall(artist_regex_pattern, channel_titles.lower())
+            #print(artists_regex_match)
+            track_regex_pattern = f"({track.lower()}){{e<=2}}"
+            track_regex_match = regex.findall(track_regex_pattern, track_names.lower())
+            #print(track_regex_match)
+            #the accuracy of the below fuzzy matching decreases as the length of the track name increases 
+            #therefore if the length of the track name exceeds 40 characters the first observation will be selected
+            if len(track) >= 40: 
+                video_id += YT_dump["items"][0]["id"]["videoId"]
+                k += len(YT_dump['items'])
+            elif (len(artists_regex_match) > 0) & (len(track_regex_match) > 0): 
+                video_id += YT_dump["items"][k]["id"]["videoId"]
+                k += len(YT_dump['items'])
+            else:
+                k += 1
+        #if all else fails then set to inital YouTube observation 
+        if video_id == '':
+            video_id += YT_dump["items"][0]["id"]["videoId"] 
+        return video_id
+
+    #Note that this method only works for tracking public playlists on my channel
+    def get_playlist_id(self, playlist_name): 
+        get_all_playlists = f'https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=5&channelId={self.KD_channel}&key={self.YouTube_API_key}'
+        playlist_pool = requests.get(get_all_playlists).json()
+        #for potential searches you can refer to the playlist_ref database
+        #DB().conn.cursor().execute("SELECT * FROM spotify.playlist_ref pr where TRIM(pr.YT_id) = ''").fetchall() 
+        for i in range(len(playlist_pool['items'])):
+            YT_playlist_title = playlist_pool['items'][0]['snippet']['title']
+            YT_playlist_id = playlist_pool['items'][0]['id']
+            playlist_regex_pattern = f"({playlist_name.lower()}){{e<=2}}"
+            playlist_regex_match = regex.findall(playlist_regex_pattern, YT_playlist_title)
+            x = 0
+            while x == 0: 
+                if len(playlist_regex_match) > 0: 
+                    response = [YT_playlist_title, YT_playlist_id]
+                else: 
+                    break
+        return response
+
+
+    #Ideally this would be a private method, but I can't get that configured so therefore this method remains public
+    def refresh_token(self, **kwargs):
+        import json, requests
+        token_path = os.getcwd()+r"\\API keys + refresh tokens\\" + "refresh_token_YouTube.json"
+        with open(token_path) as f:
+            YT_token = json.load(f)
+        refresh_token = kwargs.get("Refresh Token", YT_token['refresh_token'])
+        client_id = kwargs.get("Client ID", YT_token['client_id'])
+        client_secret = kwargs.get("Client Secret", YT_token['client_secret'])
+        url = "https://oauth2.googleapis.com/token"
+        payload = {
+            "client_id": client_id,  # Replace with your client ID
+            "client_secret": client_secret,  # Replace with your client secret
+            "refresh_token": refresh_token,
+            "grant_type": "refresh_token"
+        }
+        response = requests.post(url, data=payload)
+        response.raise_for_status()  # Raise an error for bad responses
+        return response.json()["access_token"]
+
+    def grant_access(self):
+        path = os.getcwd()+r"\\API keys + refresh tokens\\" +"client_secret_780748469530-6q5rid6ai4mh1rqgo6qt97tbmjt5iui0.apps.googleusercontent.com.json"
+        scopes = ["https://www.googleapis.com/auth/youtube.force-ssl","https://www.googleapis.com/auth/youtube","https://www.googleapis.com/auth/youtubepartner"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(path, scopes)
+        creds = flow.run_local_server(port=8080) 
+        print('Grant access')
+        with open(os.getcwd()+r"\\API keys + refresh tokens\\" + "refresh_token_YouTube.json", 'w') as token:
+            token.write(creds.to_json())
+        input('Press enter if access was granted')
+        token_path = os.getcwd()+r"\\API keys + refresh tokens\\" + "refresh_token_YouTube.json"
+        with open(token_path) as f:
+            YT_token = json.load(f)
+        YT_token = YT_token['refresh_token']
+        self.YT_token = YT_token
+        #I am perplexed by the fetch_token 
+        return YT_token
+
+    def insert_video(self, YT_video_id, YT_playlist_id): 
+        url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet"
+        headers = {
+            "Authorization": f"Bearer {YouTubeAPI().refresh_token()}",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+            }
+
+        data = {
+            "snippet": {
+                "playlistId": YT_playlist_id,
+                "resourceId": {"kind": "youtube#video",
+                    "videoId": YT_video_id
+                }
+            }
+        }
+        response =  requests.post(url, headers=headers, data=json.dumps(data))
+        return response.json()
+        #https://developers.google.com/youtube/v3/docs/playlistItems#resource
+
+    def remove_video(self, YT_playlist_list_id): 
+        url = f"https://www.googleapis.com/youtube/v3/playlistItems?id={YT_playlist_list_id}"
+        headers = {
+            "Authorization": f"Bearer {YouTubeAPI().refresh_token()}",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+            }
+        response = requests.delete(url, headers=headers)
+        return response
