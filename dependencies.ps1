6daa0a77e135c86f87d20db4814c0fe41f24366d
Kwame Doonquah <doonkwa@gmail.com>
2025-03-02 12:06:09 -0500

    Addition of powershell dependencies script

---
+++ dependencies.ps1
@@ -0,0 +1,6 @@
+python.exe -m pip install --upgrade pip
+python.exe -m pip install regex
+python.exe -m pip install google-api-python-client
+python.exe -m pip install google-auth-oauthlib
+python.exe -m pip install requests_oauthlib
+python.exe -m pip install "psycopg[binary,pool]"
