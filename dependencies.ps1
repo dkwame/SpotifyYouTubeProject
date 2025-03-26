9ebf56a2c8df2b08852689976b25aa4b69e249fb
Kwame Doonquah <doonkwa@gmail.com>
2025-03-26 18:57:08 -0400

    Revision to dependencies

--- dependencies.ps1
+++ dependencies.ps1
@@ -1,6 +1,30 @@
 python3.exe -m pip install --upgrade pip
-python3.exe -m pip install regex
-python3.exe -m pip install google-api-python3-client
-python3.exe -m pip install google-auth-oauthlib
-python3.exe -m pip install requests_oauthlib
-python3.exe -m pip install "psycopg[binary,pool]"
+$depen1 = python3.exe -m pip freeze | findstr regex
+if ($depen1 -notlike '*regex*')
+{
+    python3.exe -m pip install regex
+}
+
+$depen2 = python3.exe -m pip freeze | findstr google-api-python3-client
+if ($depen2-notlike '*google-api-python*')
+{
+    python3.exe -m pip install google-api-python3-client
+}
+
+$depen3 = python3.exe -m pip freeze | findstr google-auth-oauthlib
+if ($depen3 -notlike '*google-auth-oauthlib*')
+{
+    python3.exe -m pip install google-auth-oauthlib
+}
+
+$depen4 = python3.exe -m pip freeze | findstr requests_oauthlib
+if ($depen4 -notlike 'requests_oauthlib')
+{
+    python3.exe -m pip install requests_oauthlib
+}
+
+$depen5 = python3.exe -m pip freeze | findstr psycopg
+if ($depen5 -notlike 'psycopg-binary')
+{
+    python3.exe -m pip install "psycopg[binary,pool]"
+}
