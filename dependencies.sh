6690f625b9bd1b587869438933ceb5f97601da55
Kwame D <dkwame@Kwames-MacBook-Pro.local>
2025-03-28 16:52:48 -0400

    Addition of depenedencies for Macos

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+python3 -m pip install --upgrade pip 
+python3 -m pip install regex
+python3 -m pip install google-api-python3-client
+python3 -m pip install google-auth-oauthlib
+python3 -m pip install requests_oauthlib
+python3 -m pip install "psycopg[binary,pool]"
